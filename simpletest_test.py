8e94446e690116c759729b4a3f308b86a1c1c94a
matt-tingen <matttingen@gmail.com>
2014-10-11 15:29:34 -0400

    Converted indention to spaces per PEP8

--- simpletest_test.py
+++ simpletest_test.py
@@ -2,59 +2,59 @@ from simpletest import Test, TestFailed
 
 class SimpleTestTest(Test):
 
-	def success(self):
-		print('Passed')
+    def success(self):
+        print('Passed')
 
-	def run(self):
-		self.assert_eq(4, 4)
+    def run(self):
+        self.assert_eq(4, 4)
 
-		try:
-			self.assert_eq('test', 'diff')
-		except TestFailed:
-			pass
-		else:
-			raise AssertionError
+        try:
+            self.assert_eq('test', 'diff')
+        except TestFailed:
+            pass
+        else:
+            raise AssertionError
 
-		self.assert_lt(-2, 8)
+        self.assert_lt(-2, 8)
 
-		try:
-			self.assert_lt(28.06, 8)
-		except TestFailed:
-			pass
-		else:
-			raise AssertionError
+        try:
+            self.assert_lt(28.06, 8)
+        except TestFailed:
+            pass
+        else:
+            raise AssertionError
 
-		self.assert_len([1, 2, 3], 3)
+        self.assert_len([1, 2, 3], 3)
 
-		try:
-			self.assert_len([1, 2, 3], 4)
-		except TestFailed:
-			pass
-		else:
-			raise AssertionError
+        try:
+            self.assert_len([1, 2, 3], 4)
+        except TestFailed:
+            pass
+        else:
+            raise AssertionError
 
-		class Foo:
-			pass
-		class Baz:
-			pass
+        class Foo:
+            pass
+        class Baz:
+            pass
 
-		self.assert_instance(Foo(), Foo)
+        self.assert_instance(Foo(), Foo)
 
-		try:
-			self.assert_instance(Baz(), Foo)
-		except TestFailed:
-			pass
-		else:
-			raise AssertionError
+        try:
+            self.assert_instance(Baz(), Foo)
+        except TestFailed:
+            pass
+        else:
+            raise AssertionError
 
-		self.assert_eval('2 * half == whole', half=0.5, whole=1)
+        self.assert_eval('2 * half == whole', half=0.5, whole=1)
 
-		try:
-			self.assert_eval('2 * half == whole', half=0.5, whole=1.1)
-		except TestFailed:
-			pass
-		else:
-			raise AssertionError
+        try:
+            self.assert_eval('2 * half == whole', half=0.5, whole=1.1)
+        except TestFailed:
+            pass
+        else:
+            raise AssertionError
 
 TestTest()
 
